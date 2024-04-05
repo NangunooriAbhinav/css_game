463e212aa8b817127123f1c0c1c471e213e6dbb8
NangunooriAbhinav <abhinavnangunoori.29@gmail.com>
2024-04-05 20:39:38 +0530

    initial verion complete

--- app/components/codeinput.tsx
+++ app/components/codeinput.tsx
@@ -1,7 +1,14 @@
 "use client";
+import { on } from "events";
 import React, { useState } from "react";
 
-export default function CodeInput() {
+export default function CodeInput({
+  mode,
+  onSubmit,
+}: {
+  mode: number;
+  onSubmit: (input: string) => void;
+}) {
   const [cssCode, setCssCode] = useState<string>("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -9,19 +16,43 @@ export default function CodeInput() {
   };
 
   const handleSubmit = () => {
-    // You can perform actions with the entered CSS code here
-    console.log(cssCode);
+    onSubmit(cssCode);
   };
 
   return (
     <div className="container w-50 d-flex flex-column justify-content-between">
       <h5 className="my-2 text-white">Enter Your CSS Code</h5>
-      <textarea
+      <div
         className="code-input leetcode-input"
-        placeholder="Enter your CSS code here..."
-        value={cssCode}
-        onChange={handleInputChange}
-      ></textarea>
+        style={{
+          minHeight: "200px",
+          border: "1px solid #ced4da",
+          padding: "10px",
+          borderRadius: "4px",
+          backgroundColor: "#000",
+          color: "#495057",
+          overflow: "auto",
+        }}
+      >
+        {/* Permanent text */}
+        <pre className="m-0">element.class{"{"}</pre>
+        <pre>display:{mode <= 5 ? "grid" : "flex"}</pre>
+        {/* Editable text */}
+        <textarea
+          style={{
+            width: "100%",
+            border: "none",
+            outline: "none",
+            resize: "none",
+            background: "transparent",
+            color: "#fff",
+          }}
+          placeholder="Enter your CSS code here..."
+          value={cssCode}
+          onChange={handleInputChange}
+        ></textarea>
+        <pre>{"}"}</pre>
+      </div>
       <button className="submit-btn btn w-100" onClick={handleSubmit}>
         Submit
       </button>
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,96 @@ import { useState } from "react";
 
 export default function Home() {
   const [cssMode, setCssMode] = useState(1);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [wrongSubmit, setWrongSubmit] = useState(false);
+
+  const answers = [
+    "grid-template-columns: 1fr 1fr 1fr;",
+    "grid-template-columns: 1fr 2fr 1fr;",
+    "grid-template-columns: 1fr 2fr;",
+    "grid-template-columns: 1fr 1fr 1fr;grid-template-rows: 1fr 2fr;",
+    "grid-template-columns: 1fr 2fr;grid-auto-rows: 1fr 2fr;",
+    "justify-content: center;",
+    "justify-content: center;align-items: center;",
+    "justify-content: space-between;align-items: center;",
+    "justify-content: space-around;align-items: center;",
+    "justify-content: center;align-items: flex-end;",
+  ];
 
   const handleModeChange = () => {
-    setCssMode((prev) => (prev === 10 ? 1 : prev + 1));
+    if (progress === cssMode) {
+      setCssMode((prev) => (prev === 10 ? 1 : prev + 1));
+    } else {
+      setWrongSubmit(true);
+    }
+  };
+
+  const handleSubmit = (input: string) => {
+    input = input.replace(/\n/g, "");
+    if (input === answers[cssMode - 1]) {
+      console.log("Correct!");
+      setIsCorrect(true);
+      setProgress((prev) => (prev === 9 ? 0 : prev + 1));
+    } else {
+      console.log("Incorrect!");
+      setWrongSubmit(true);
+    }
+    console.log(input);
   };
 
   return (
     <>
       <section className="main d-flex justify-content-center align-items-center">
+        {isCorrect && (
+          <div
+            className="modal show d-flex justify-content-center align-items-center"
+            style={{ display: "block" }}
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">Congratulations!</h5>
+                  <button
+                    type="button"
+                    className="btn btn-close "
+                    onClick={() => setIsCorrect(false)}
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <p>You gave the correct answer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+        {wrongSubmit && (
+          <div
+            className="modal show d-flex justify-content-center align-items-center"
+            style={{ display: "block" }}
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title">
+                    Please finish the Current Stage!
+                  </h5>
+                  <button
+                    type="button"
+                    className="btn btn-close "
+                    onClick={() => setWrongSubmit(false)}
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <p>You gave the wrong answer.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
         <div className="game-container d-flex p-3 ">
           <View cssDetails={cssMode} />
-          <CodeInput />
+          <CodeInput mode={cssMode} onSubmit={handleSubmit} />
           <button onClick={handleModeChange} className="btn border switch">
             {">"}
           </button>
